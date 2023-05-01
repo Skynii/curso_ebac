94c8cc698f64cd3ff0e643a04cea1d5ca9664966
Érica Joice dos Santos <ejoice19@gmail.com>
2023-05-01 09:16:57 -0300

    pegando numeros corretamente

---
+++ exercicioJs/main.js
@@ -0,0 +1,8 @@
+const numInput1 = document.getElementById('num1');
+const numInput2= document.getElementById('num2');
+const btnValidar = document.getElementById('btn');
+
+function verificarNumeros() {
+   let num1 = parseInt(numInput1.value);
+   console.log(num1);
+}

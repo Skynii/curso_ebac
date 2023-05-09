eaefdcab4a9b733aa0522a9df904dc2f2352c0ec
Érica Joice dos Santos <ejoice19@gmail.com>
2023-05-09 09:36:08 -0300

    funcionando.

--- exercicioJs/main.js
+++
@@ -1,8 +0,0 @@
-const numInput1 = document.getElementById('num1');
-const numInput2= document.getElementById('num2');
-const btnValidar = document.getElementById('btn');
-
-function verificarNumeros() {
-   let num1 = parseInt(numInput1.value);
-   console.log(num1);
-}

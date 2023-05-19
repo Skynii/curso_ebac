2cccb75a55560d73c99a5d408ece636ab315c533
Érica Joice dos Santos <ejoice19@gmail.com>
2023-05-19 12:28:16 -0300

    teste de verificação de input atraves do alert. ok!

--- projeto3/main.js
+++ projeto3/main.js
@@ -1,4 +1,9 @@
 const form = document.getElementById('form-atividade');
 form.addEventListener('submit', function(e) {
-   e.preventDefault();ddd
+   e.preventDefault();
+
+   const inputNomeAtividade = document.getElementById('nomeDaAtividade');
+   const inputNotaAtividade = document.getElementById('notaDaAtividade');
+
+   alert(`Atividade ${inputNomeAtividade.value} - Nota ${inputNotaAtividade.value}`);
 })

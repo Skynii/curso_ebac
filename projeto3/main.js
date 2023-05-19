6de152674dc5adecb1bc8000b44bbe4c6d57fec1
Érica Joice dos Santos <ejoice19@gmail.com>
2023-05-19 16:25:44 -0300

    Esta adicionando nova linha

--- projeto3/main.js
+++ projeto3/main.js
@@ -1,20 +1,22 @@
 const form = document.getElementById('form-atividade');
+let linhas = '';
 form.addEventListener('submit', function(e) {
    e.preventDefault();
 
    const inputNomeAtividade = document.getElementById('nomeDaAtividade');
    const inputNotaAtividade = document.getElementById('notaDaAtividade');
 
-   //Adicionando  no corpo da tabela
-
+   
    let linha = '<tr>';
    linha += `<td>${inputNomeAtividade.value}</td>`;
    linha += `<td>${inputNotaAtividade.value}</td>`;
    linha += `<td>${inputNotaAtividade.value >=7 ? 'Aprovado' : 'Reprovado'}</td>`;
    linha += `</tr>`;
 
+   linhas += linha;
+
    const corpoTabela = document.querySelector('tbody');
-   corpoTabela.innerHTML = linha;
+   corpoTabela.innerHTML = linhas;
 });
 
 

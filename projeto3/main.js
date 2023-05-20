9987fe7093120fa35ee9b0ecd51d66e3eb7c0e5f
Érica Joice dos Santos <ejoice19@gmail.com>
2023-05-20 10:12:01 -0300

    Automatização de calculo de media

--- projeto3/main.js
+++ projeto3/main.js
@@ -3,6 +3,8 @@ const imgAprovado = '<img src="./images/aprovado.png" alt="Emoji celebrando">';
 const imgReprovado = '<img src="./images/reprovado.png" alt="Emoji decepcionado">';
 const atividades = [];
 const notas = [];
+const spanAprovado = '<span class="resultado aprovado">Aprovado</span>';
+const spanReprovado = '<span class="resultado reprovado">Reprovado</span>';
 
 let linhas = '';
 
@@ -40,11 +42,19 @@ function atualizaTabela() {
 }
 
 function atualizaMediaFinal() {
+   const mediaFinal = calculaMediaFinal();
+   
+   document.getElementById('media-final-valor').innerHTML = mediaFinal;
+   document.getElementById('media-final-resultado').innerHTML = mediaFinal >= 7 ? spanAprovado : spanReprovado;
+   
+}
+
+function calculaMediaFinal() {
    let somaDasNotas = 0;
 
    for(let i = 0; i <notas.length; i++){
       somaDasNotas += notas[i];
    }
-   console.log(atividades);
-   console.log(somaDasNotas); 
+
+   return somaDasNotas/notas.length;
 }

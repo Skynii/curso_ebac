ce2081ddb63930fa1a159764f94ad4b9e2bef2fc
Érica Joice dos Santos <ejoice19@gmail.com>
2023-05-23 00:05:08 -0300

    uptade: validação de dados repetidos

--- Agenda/main.js
+++ Agenda/main.js
@@ -1,11 +1,14 @@
 
 const form = document.querySelector('#form-agenda');
+const nomes = [];
+const telefones = [];
 
 let linhas = '';  
 
 form.addEventListener('submit', function(e) {
    e.preventDefault();
    adicionarContato();
+   incluirDados();
    
 });  
 
@@ -15,15 +18,31 @@ function adicionarContato(){
    const email= document.getElementById('email');
    const categoria= document.getElementById('categoria');
 
-   let linha = '<tr>';
+   
 
-   linha += `<td>${nome.value}</td>`;
-   linha += `<td>${tel.value}</td>`;
-   linha += `<td>${categoria.value}</td>`;
-   linha += `<td>${email.value}</td>`;
-   linha += `</td>`
+   if (nomes.includes(nome.value) ){
+      alert(`Contato já adicionado anteriormente!`)
+   } else if(telefones.includes(tel.value)){
+      alert(`Telefone adicionado anteriormente!`)
+   }else{
+      nomes.push(nome.value);
+      telefones.push(tel.value);
+      let linha = '<tr>';
+      linha += `<td>${nome.value}</td>`;
+      linha += `<td>${tel.value}</td>`;
+      linha += `<td>${categoria.value}</td>`;
+      linha += `<td>${email.value}</td>`;
+      linha += `</tr>`
 
-   linhas += linha;
+      linhas += linha;}
+      
 
    [nome.value, tel.value, email.value, categoria.value] =['','','',''];
+   console.log(telefones)
+}
+
+function incluirDados(){
+   const corpoAgenda = document.querySelector('tbody');
+
+   corpoAgenda.innerHTML = linhas;
 }

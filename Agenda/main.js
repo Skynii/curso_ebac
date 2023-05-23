70a6a52e601a36003338c50501380319f51833d1
Érica Joice dos Santos <ejoice19@gmail.com>
2023-05-22 23:45:57 -0300

    update: input sendo apagado após envio do formulário

--- Agenda/main.js
+++ Agenda/main.js
@@ -5,22 +5,25 @@ let linhas = '';
 
 form.addEventListener('submit', function(e) {
    e.preventDefault();
-   adicionaContato();
+   adicionarContato();
    
 });  
 
-function adicionaContato(){
+function adicionarContato(){
    const nome= document.getElementById('nome');
    const tel= document.getElementById('tel');
    const email= document.getElementById('email');
    const categoria= document.getElementById('categoria');
 
-   console.log(nome.value)
    let linha = '<tr>';
+
    linha += `<td>${nome.value}</td>`;
    linha += `<td>${tel.value}</td>`;
    linha += `<td>${categoria.value}</td>`;
    linha += `<td>${email.value}</td>`;
    linha += `</td>`
-   
+
+   linhas += linha;
+
+   [nome.value, tel.value, email.value, categoria.value] =['','','',''];
 }

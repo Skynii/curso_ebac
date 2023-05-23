7109edae33d03e6e4f5507bc469374831c15c8fb
Érica Joice dos Santos <ejoice19@gmail.com>
2023-05-22 23:22:34 -0300

    pegando os inputs ok

--- Agenda/main.js
+++ Agenda/main.js
@@ -1,13 +1,26 @@
 
 const form = document.querySelector('#form-agenda');
 
+let linhas = '';  
+
 form.addEventListener('submit', function(e) {
    e.preventDefault();
+   adicionaContato();
+   
+});  
 
+function adicionaContato(){
    const nome= document.getElementById('nome');
    const tel= document.getElementById('tel');
    const email= document.getElementById('email');
    const categoria= document.getElementById('categoria');
 
    console.log(nome.value)
-});   
+   let linha = '<tr>';
+   linha += `<td>${nome.value}</td>`;
+   linha += `<td>${tel.value}</td>`;
+   linha += `<td>${categoria.value}</td>`;
+   linha += `<td>${email.value}</td>`;
+   linha += `</td>`
+   
+}

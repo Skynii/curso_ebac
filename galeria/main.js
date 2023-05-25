86773139d043a7e16fcd22ca903521a406a1e9bb
Érica Joice dos Santos <ejoice19@gmail.com>
2023-05-24 23:57:50 -0300

    lançando evento no botao

--- galeria/main.js
+++ galeria/main.js
@@ -2,4 +2,11 @@ $(document).ready(function(){
    console.log(document.querySelectorAll('header button'));
    console.log($('header button'));
 
+   document.querySelector('header button').addEventListener('click', function(){
+
+
+   });
+   $('header button').click(function(){
+      alert("Expandir formulário")
+   })
 })

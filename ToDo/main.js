6b5eb6d52928039e166b126116c5e0e56fa91643
Érica Joice dos Santos <ejoice19@gmail.com>
2023-06-04 09:19:27 -0300

    to-do List finalizado

--- ToDo/main.js
+++ ToDo/main.js
@@ -1,12 +1,17 @@
 $(document).ready(function() {
-   $('#btn-add').click(function() {
-      console.log('botãoclicado')
-   })
-})
+   $('#btn-add').click(function(e) {
+      e.preventDefault();
 
-$('form').on('submit', function(e) {
-   e.preventDefault();
-
-   const tarefaNova = $('#todo').val();
-   console.log(tarefaNova)
-})
+      const tarefaNova = $('#todo').val();
+      console.log(tarefaNova);
+      
+      const novoItem = $('<li></li>'); // Cria um novo elemento <li>
+      const checkbox = $('<input type="checkbox" class="check">'); // Cria um novo elemento <input>
+      const label = $('<label></label>').text(tarefaNova); // Cria um novo elemento <label> e define o texto
+      
+      novoItem.append(checkbox); // Adiciona o elemento <input> ao elemento <li>
+      novoItem.append(label); // Adiciona o elemento <label> ao elemento <li>
+      
+      $('.lista').append(novoItem); // Adiciona o novo item à lista de tarefas
+   });
+});

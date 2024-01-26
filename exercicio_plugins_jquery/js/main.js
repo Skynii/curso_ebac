0caf93ac014f27c015be9fc6de244b83e5ceaf5d
Érica Joice dos Santos <ejoice19@gmail.com>
2024-01-26 13:35:24 -0300

    console contando quantos campos estão faltando

--- exercicio_plugins_jquery/js/main.js
+++ exercicio_plugins_jquery/js/main.js
@@ -6,4 +6,36 @@ $(document).ready(function() {
    $('#cpf').mask('000.000.000-00')
 
    $('#cep').mask('00000-000')
+
+   $('#nascimento').mask('00/00/0000')
+
+   //Validando o formulário//
+   $('form').validate({
+      rules: {
+         nome: {
+            required: true
+         },
+         cpf:{
+            required: true
+         },
+         nascimento:{
+            required: true
+         },
+         email:{
+            required: true,
+            email: true
+         }
+      },
+      messages: {
+         nome:'Por favor coloque seu nome.',
+         email:'Por favor, verifique o campo e-mail'
+      },
+      submitHandler: function(form) {
+         console.log(form)
+      },
+      invalidHandler: function(evento, validador) {
+         let camposIncorretos =validador.numberOfInvalids()
+         console.log(camposIncorretos)
+      }
+   })
 })
